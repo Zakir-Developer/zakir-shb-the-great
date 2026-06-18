8b1cba322db2871b8b6f7c6d2a13b81a5006f5f5
zakir <zakirmunir84@gmail.com>
2026-06-18 08:28:57 -0400

    wr are creating a conditon

--- first.py
+++ first.py
@@ -1,2 +1,6 @@
-print("hello world!")
-print("Data Analytics Course")
+import random
+number = random.randint()
+if number < 100:
+    print(number , "is less than 100")
+else:
+    print(number , "is greater than 100")
